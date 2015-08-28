68b2755f92accc4249962f33ebd2121353b669fa
mlee0330 <michaelyurilee@gmail.com>
2015-08-28 16:17:00 -0700

    Finished filter, reject, uniq, map and reduce functions.

--- src/underbar.js
+++ src/underbar.js
@@ -81,16 +81,34 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var passed = [];
+    _.each(collection, function(item) {
+      if(test(item)){
+        passed.push(item);
+      }
+    });
+    return passed;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var failed =  _.filter(collection, function(item){
+      return (!test(item));
+    });
+    return failed;
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var uniqueItems = [];
+    _.each(array, function(item) {
+      if (_.indexOf(uniqueItems, item) === -1) {
+        uniqueItems.push(item);
+      }
+    });
+    return uniqueItems;
   };
 
 
@@ -99,6 +117,11 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var results = [];
+    _.each(collection, function(item){
+      results.push(iterator(item));
+    });
+    return results;
   };
 
   /*
@@ -140,6 +163,16 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    var noAccumulator = (arguments.length < 3);
+    _.each(collection, function(item){
+      if(noAccumulator){
+        accumulator = item;
+        noAccumulator = false;
+      } else {
+        accumulator = iterator(accumulator, item);
+      }
+    });
+    return accumulator;
   };
 
   // Determine if the array or object contains a given value (using `===`).
